0a7a71256ff1df664416a8164bc190e24be92c44
Tre-brock <ebrocklesby77@gmail.com>
2024-04-07 16:58:21 -0400

    Added a message about gmail.

--- workaround/Gmail.js
+++ workaround/Gmail.js
@@ -1,6 +1,6 @@
 
 const replacements = [
-    { from: "Javascript must be enabled in order for you to use Gmail. However, it seems Javascript is either disabled or not supported by your browser. Learn more at our", to: "<h1>To use gmail without Javascript I recommend thunderbird mail</h1> <p>(aka icedove)</p><h1> It is free sofwtare</h1> (besides the logo, and that is why icedove exist)<br><a href=https://www.thunderbird.net/en-US/download/> <h1>Thunderbird Download</h1> <br> <br> <br><br><br> <br> <br><br><hr> " },
+    { from: "Javascript must be enabled in order for you to use Gmail. However, it seems Javascript is either disabled or not supported by your browser. Learn more at our", to: "<h1>To use email without nonfree Javascript I recommend thunderbird mail, It is free sofwtare</h1><br><a href=https://www.thunderbird.net/en-US/download/> <h1>Thunderbird Download</h1> <br> <br> <br><br><br> <br> <br><br><hr> " },
     
   ];
 
